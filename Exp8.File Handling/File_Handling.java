2e7320749ad3539767b467692f61c363509029c0
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2023-11-29 14:25:53 +0530

    new commit

---
+++ Exp8.File Handling/File_Handling.java
@@ -0,0 +1,6 @@
+/*Template
+Implement a generic program using any collection class
+ to count the number of elements in a collection that
+  have a specific property such as even numbers, 
+  odd number, prime number and palindromes.
+ */
