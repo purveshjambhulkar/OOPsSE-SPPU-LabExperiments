2fa2c95885218819ba8cfa1ec21694382382466a
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2023-11-29 15:38:35 +0530

    new commit

--- Exp6.Exception Handling/Exception_Handling.java
+++ Exp6.Exception Handling/Exception_Handling.java
@@ -9,4 +9,36 @@ Implement a program to handle Arithmetic exception,
      Display the exception.
  */
 
- 
+import java.util.Scanner;
+
+public class Exception_Handling {
+
+    public static void main(String[] args) {
+
+        // Array index out of the bound
+        try {
+            int arr[] = new int[10];
+            arr[15] = 5;// this will throw error
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException : " + e);
+        }
+
+        // division exception handling
+
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter num 1");
+            int num1 = Integer.parseInt(input.next());
+            System.out.println("Enter num 2");
+            int num2 = Integer.parseInt(input.next());
+            int result = num1/num2 ;
+        } catch (ArithmeticException e) {
+            System.out.println("Denominator in Zero : "+e);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Input is invalid : "+e);
+        }
+
+    }
+}
